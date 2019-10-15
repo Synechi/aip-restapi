bcda0236b82d9bb8b272e7df3876ec8c76d6d8c1
Christian <43329131+CTKNguyen@users.noreply.github.com>
2019-10-15 17:43:57 +1100

    revert back to old image upload code

--- services/image-upload.js
+++ services/image-upload.js
@@ -1,39 +1,28 @@
-// code made with multer-imamger usage example: https://www.npmjs.com/package/multer-imager
+// Code from a tutorial made by Filip Jerga: https://www.youtube.com/watch?v=ASuU4km3VHE
+// Git repo containing code: https://github.com/Jerga99/book-with-me-ng/blob/master/server/services/file-upload.js
 
+var aws = require('aws-sdk');
 var multer = require('multer');
-var imager = require('multer-imager');
-
-// Images are upload to amazon S3 through multer-imager
-const upload = multer({
-  storage: imager({
-    dirname: 'image',
-    bucket: 'aip-project2019',
+var multerS3 = require('multer-s3');
+ 
+// Keys for amazon S3 set kere
+aws.config.update({
     secretAccessKey: 'Qc7udhJTDnK87Sl5KROPwBQgKfMwHULaGqnxumVL',
     accessKeyId: 'AKIAIWMXUT3OEDRA7UXA',
-    region: 'ap-southeast-2',
+    region: 'ap-southeast-2'
+});
 
-    // Define graphicsmagick options
-    gm: {                                 
-      // Define the maximum dimensions of the image
-      // Images are expanded or contracted to fit these values whilist keeping aspect ratio
-      // More info in graphicsmagick doc: inhttp://www.graphicsmagick.org/GraphicsMagick.html#details-geometry
-      width: 1000,                
-      height: 1000, 
+const s3 = new aws.S3();
 
-      // Crop the image to the changed dimensions
-      // Crop field is required for graphicsmagick to work
-      // More info in graphicsmagick doc: http://www.graphicsmagick.org/GraphicsMagick.html#details-crop
-      crop: {                   
-        width: 1000,
-        height: 1000,
-        x: 0,
-        y: 0
-      },
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'aip-project2019',
+    acl: 'public-read',           // Allow image URL to be publicly accessed
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: 'TESTING_META_DATA!'});
     },
-    s3 : {
-      ACL: 'public-read'          // Allow image to be publicly read
-    },
-    filename: function (req, file, cb) {
+    key: function (req, file, cb) {
       cb(null, Date.now().toString())         // Set file key to be the current date and time
     }
   })
